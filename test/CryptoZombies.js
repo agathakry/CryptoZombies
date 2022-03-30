91bbebdb36519a3a15f9e2462e7223ef31c9cbbd
agathakry <agathakrysti@gmail.com>
2022-03-30 19:00:32 +0100

    First test

---
+++ test/CryptoZombies.js
@@ -0,0 +1,13 @@
+// 1. Lesson 11 Chapter 2
+// first line of code should declare a const named CryptoZombies and set it equal 
+// to result of the artifacts.require function with name of the contract we want to 
+// test as an argument 
+
+const CryptoZombies = artifacts.require("CryptoZombies");
+
+// 2. Write a test for the contract 
+// 3. it() is executing test text 
+contract("CryptoZombies", (accounts) => {
+    it("should be able to create a new zombie", () => {     
+    })
+})
