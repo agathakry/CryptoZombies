54ce2585e4006d2a730d453d360f5c3668f48e81
agathakry <agathakrysti@gmail.com>
2022-04-01 18:48:30 +0100

    assertions improved

--- test/testZombieFactory.js
+++ test/testZombieFactory.js
@@ -6,6 +6,7 @@
 const ZombieFactory = artifacts.require("./zombiefactory.sol");
 // for not throwing errors
 const utils = require("./helpers/utils");
+const utils = require("./helpers/time");
 const zombieNames = ["Zombie 1", "Zombie 2"];
 // 2. Write a test for the contract 
 // 3. it() is executing test text 
@@ -59,7 +60,7 @@ contract("ZombieFactory", (accounts) => {
         // B. declare zombieId and set it equal to zombie id 
         const zombieId = result.logs[0].args.zombieId.toNumber();
         // C. transfer from alice and bob 
-        await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+        await contractInstance.transferFrom(alice, bob, zombieId,{from: alice});
         // D. declare newowner and set equal ownerOf with zombieId
         const newOwner = await contractInstance.ownerOf(zombieId);
         // E. Check whether Bob owns the ERC271 token 
@@ -70,12 +71,43 @@ contract("ZombieFactory", (accounts) => {
     xcontext("with the two-step transfer scenario", async () => {
         it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
         // TODO: Test the two-step scenario.  The approved address calls transferFrom
+        // Chapter 10: 2 steps
+        // A. first lines similar to 1 step
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from:alice});
+        const zombieId = result.logs[0].args.zombieId.toNumber();
+        // to approve bob call approve and make sure alice method 
+        await contractInstance.approve(bob, zombieId, {from: alice});
+
+        // last three lines similar
+        await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
+        const newOwner = await contractInstance.ownerOf(zombieId);
+        assert.equal(newOwner, bob);
+        // 
 
 
         })
         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
             // TODO: Test the two-step scenario.  The owner calls transferFrom
+            // Chapter 11: alice calls transfer from
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        const zombieId = result.logs[0].args.zombieId.toNumber();
+        await contractInstance.approve(bob, zombieId, {from: alice});
+        await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+        const newOwner = await contractInstance.ownerOf(zombieId);
+        assert.equal(newOwner,bob);
         })
+    })
+    // Chapter 12: Time travel and zombie attack
+    it("zombies should be able to attack another zombie", async () => {
+        let result;
+        result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        const firstZombieId = result.logs[0].args.zombieId.toNumber();
+        result = await contractInstance.createRandomZombie(zombieNames[1], {from: bob});
+        const secondZombieId = result.logs[0].args.zombieId.toNumber();
+        //TODO: increase the time
+        await time.increase(time.duration.days(1))
+        await contractInstance.attack(firstZombieId, secondZombieId, {from: alice});
+        assert.equal(result.receipt.status, true);
     })
 
 })
